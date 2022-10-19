08707c162979e0fbb4d365cb3db556195cc3b889
LuanCalais <luancalais@gmail.com>
2022-10-19 20:54:52 -0300

    start function works

--- src/components/timerComp.vue
+++ src/components/timerComp.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <stop-watch :timeSeconds="timeSeconds" />
-    <primary-button icon="bx bx-play" text="play" @clicked="start" :disabled="stopWatchInit"/>
-    <primary-button icon="bx bx-pause" text="stop" @clicked="stop" :disabled="!stopWatchInit"/>
+    <primary-button
+      icon="bx bx-play"
+      text="play"
+      @clicked="start"
+      :disabled="stopWatchInit"
+    />
+    <primary-button
+      icon="bx bx-pause"
+      text="stop"
+      @clicked="stop"
+      :disabled="!stopWatchInit"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import stopWatch from "./stopWatch.vue";
-import primaryButton from "./primaryButton.vue"
+import primaryButton from "./primaryButton.vue";
 
 export default defineComponent({
   name: "timerComp",
-  data(){
-    return{
+  data() {
+    return {
       timeSeconds: 0,
-      stopWatchInit: false
-    }
+      stopWatch: 0, //Cont 
+      stopWatchInit: false,
+    };
   },
 
   components: {
     "stop-watch": stopWatch,
-    "primary-button": primaryButton
+    "primary-button": primaryButton,
   },
 
   methods: {
     start() {
-      this.stopWatchInit = true
-      alert('Start')
+      this.stopWatchInit = true;
+
+      // Increment 1++ in 1 second
+      this.stopWatch = setInterval(() => {
+        this.timeSeconds++;
+      }, 1000);
     },
 
     stop() {
-      this.stopWatchInit = false
-      alert('Stop')
-    }
-  }
-
+      this.stopWatchInit = false;
+      this.stopWatch = 0
+    },
+  },
 });
 </script>
 
