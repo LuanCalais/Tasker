715d0c2e7a6f65cba4e1ff168a141bc430e57dae
LuanCalais <luancalais@gmail.com>
2022-11-22 20:29:01 -0300

    create new project mutation

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,21 +4,35 @@ import { createStore, Store, useStore as VuexUseStore } from "vuex";
 import type IProject from "@/components/interfaces/IProject";
 import { CREATE_PROJECT } from './typeMutations';
 
-interface Estate{
-    projects: IProject
+interface State{
+    projects: IProject[]
 }
 
-export const key: InjectionKey<Store<Estate>> = Symbol()
+export const key: InjectionKey<Store<State>> = Symbol()
 
-export const store = createStore<Estate>({
+export const store = createStore<State>({
     state:{
         projects: []
     },
 
     mutations: {
         [CREATE_PROJECT](state, nameProject: string){
-            // @Luan--- Continuar daqui
+            const project = {
+                id: new Date().toISOString(),
+                name: nameProject
+            } as IProject
+            
+            state.projects.push(project)
         }
     }
 
 })
+
+export function useStore(): Store<State>{
+    return VuexUseStore(key)
+}
+
+
+
+
+
--- src/views/Projects/FormProject.vue
+++ src/views/Projects/FormProject.vue
@@ -1,47 +1,58 @@
 <template>
-    <section>
-        <form @submit.prevent="save">
-            <div class="field">
-                <label for="nameOfProject" class="label">Nome do Projeto</label>
-                <input type="text" class="input" v-model="nameProject" id="nameOfProject">
-            </div>
-            <div class="field">
-                <button class="button" type="submit">Salvar</button>
-            </div>
-        </form>
-    </section>
+  <section>
+    <form @submit.prevent="save">
+      <div class="field">
+        <label for="nameOfProject" class="label">Nome do Projeto</label>
+        <input
+          type="text"
+          class="input"
+          v-model="nameProject"
+          id="nameOfProject"
+        />
+      </div>
+      <div class="field">
+        <button class="button" type="submit">Salvar</button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script lang="ts">
-
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import { useStore } from "@/store";
+import { CREATE_PROJECT } from "@/store/typeMutations";
 
 export default defineComponent({
-    name: 'FormProject',
-    props:{
-        id: {
-            type: String
-        }
+  name: "FormProject",
+  props: {
+    id: {
+      type: String,
     },
+  },
 
-    mounted(){
-        if(this.id){
-            console.log('Tem id')
-        }
-    },
-
-    data(){
-        return{
-            nameProject: ''
-        }
-    },
-
-    methods:{
-        save(){
-            alert(`O nome do seu projeto é ${this.nameProject}`)
-        }
+  mounted() {
+    if (this.id) {
+      console.log("Tem id");
     }
-})
+  },
 
+  data() {
+    return {
+      nameProject: "",
+    };
+  },
 
+  methods: {
+    save() {
+      this.store.commit(CREATE_PROJECT, this.nameProject);
+    },
+  },
+
+  setup() {
+    const store = useStore();
+    return {
+      store,
+    };
+  },
+});
 </script>
