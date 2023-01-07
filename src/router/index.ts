e4d069102fb1ee85dc02fe701377c040fc886f0a
LuanCalais <luancalais@gmail.com>
2023-01-07 13:46:56 -0300

    create files homepage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,49 @@
-import { createRouter, createWebHashHistory, type RouteRecordRaw } from "vue-router";
-import Tasks from '@/views/Tasks.vue'
-import Projects from '@/views/Projects.vue'
-import List from '@/views/Projects/List.vue'
-import FormProject from '@/views/Projects/FormProject.vue'
-
+import {
+  createRouter,
+  createWebHashHistory,
+  type RouteRecordRaw,
+} from "vue-router";
+import Tasks from "@/views/Tasks.vue";
+import Projects from "@/views/Projects.vue";
+import List from "@/views/Projects/List.vue";
+import FormProject from "@/views/Projects/FormProject.vue";
+import HomePage from "@/views/homePage/index.vue";
 
 // RouteRecordRaw - interfate to make the app routes
-const routes: RouteRecordRaw[] = [{
-    path: '/',
-    name: 'Tasks',
-    component: Tasks 
-},{
-    path: '/projects',
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Tasks",
+    component: Tasks,
+  },
+
+  {
+    path: "",
+    name: "HomePage",
+    component: HomePage,
+  },
+  {
+    path: "/projects",
     component: Projects,
 
     children: [
-        {
-            path: '',
-            name: 'Projects',
-            component: List
-        },
-        {
-            path: 'new',
-            name: 'New Project',
-            component: FormProject
-        }
-    ]
-
-}]
+      {
+        path: "",
+        name: "Projects",
+        component: List,
+      },
+      {
+        path: "new",
+        name: "New Project",
+        component: FormProject,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: routes
-})
+  history: createWebHashHistory(),
+  routes: routes,
+});
 
 export default router;
---
+++ src/views/homePage/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Home Page works</h1>
+</template>
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: "HomePage"
+    })
+
+</script>
+
+<style src="./style.css" scoped></style>
+
